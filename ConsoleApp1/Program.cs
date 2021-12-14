63034fd0db4bb86fda0a67ed1e47d101779108d8
priyanka <priyankamukhi96@gmail.com>
2021-12-14 18:14:47 +0530

    initail commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write((j)+"");
+                }      
+
+                Console.WriteLine();
+            }
+            for(int i=1;i<=4;i++)
+            {
+                for(int j=4;j>=i;j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
